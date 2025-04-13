0b89ac6e9a4169c29de686e4ad1ed7532582d7b3
Dharmesh Tarapore <dharmesh@tarapore.ca>
2025-04-13 12:11:43 -0700

    Updated to account for query parameters.

--- oldwiki.js
+++ oldwiki.js
@@ -1,5 +1,11 @@
-if(!window.location.href.includes("?useskin=vector")) {
-	window.location.replace(document.URL + "?useskin=vector");
+// Check if the current page is a Wikipedia page and doesn't already use Vector skin
+if (window.location.hostname.includes('wikipedia.org') && !window.location.href.includes('useskin=vector')) {
+    // Check if there are already query parameters in the URL
+    if (window.location.href.includes('?')) {
+        // Append the vector skin parameter with an ampersand
+        window.location.replace(document.URL + '&useskin=vector');
+    } else {
+        // Add the vector skin parameter with a question mark
+        window.location.replace(document.URL + '?useskin=vector');
+    }
 }
-
-
